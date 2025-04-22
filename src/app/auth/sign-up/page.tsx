6494a0a434d6fe5c45ecd9f0004b50a9c024be59
Vlad Khrmov <vlad.k@saasjet.com>
2025-04-22 20:34:14 +0300

    feat: add google auth

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -7,6 +7,7 @@ import { Checkbox } from '@src/components/ui/checkbox';
 import { Input } from '@src/components/ui/input';
 import { Label } from '@src/components/ui/label';
 import PasswordField from '@src/components/ui/password-field';
+import { signIn } from 'next-auth/react';
 
 export default function SignUpPage() {
   const initialValues = {
@@ -133,7 +134,7 @@ export default function SignUpPage() {
       </div>
 
       <div className="grid grid-cols-1 gap-4">
-        <Button variant="outline">
+        <Button variant="outline" onClick={() => signIn('google')}>
           <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
             <path
               d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
